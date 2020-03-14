61ee8e26b19d7250472c2257729b89068846bc43
xjr <627893908@qq.com>
2020-03-15 03:46:54 +0800

    03.15

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,8 +1,35 @@
 <template>
+	<view class="">
+		<view class="">
+			
+		</view>
+	</view>
 </template>
 
 <script>
+	import Xuanran from "../xuanran/xuanran"
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		computed: {
+	
+	
+		},
+		components: {
+			
+		},
+		created() {
+			
+		}
+	}
 </script>
 
 <style>
+	
 </style>
